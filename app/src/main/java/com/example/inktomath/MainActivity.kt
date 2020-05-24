3a0f79ac6c28d1b73e4f47d7fb2e674616ec6be4
am15h <amishgarg123@gmail.com>
2020-05-25 00:26:18 +0530

    add MainActivity

--- app/src/main/java/com/example/inktomath/MainActivity.kt
+++ app/src/main/java/com/example/inktomath/MainActivity.kt
@@ -8,7 +8,11 @@ import android.util.Log
 import android.view.MotionEvent
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.divyanshu.draw.widget.DrawView
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +20,9 @@ class MainActivity : AppCompatActivity() {
     private var clearButton: Button? = null
     private var classifyButton: Button? = null
     private var predictedTextView: TextView? = null
-    private var digitClassifier = DigitClassifier(this)
+    private var equationText: TextView? = null
+    private var digitClassifier = SymbolClassifier(this)
+    private var calculate_button: Button? = null
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,43 +31,74 @@ class MainActivity : AppCompatActivity() {
 
         // Setup view instances
         drawView = findViewById(R.id.draw_view)
-        drawView?.setStrokeWidth(70.0f)
+        drawView?.setStrokeWidth(55.0f)
         drawView?.setColor(Color.WHITE)
         drawView?.setBackgroundColor(Color.BLACK)
         clearButton = findViewById(R.id.clear_button)
         predictedTextView = findViewById(R.id.predicted_text)
         classifyButton = findViewById(R.id.classify_button)
+        equationText = findViewById(R.id.equation_text)
+        calculate_button = findViewById(R.id.calculate_button)
+
+
+        var eqn = ""
 
         // Setup clear drawing button
         clearButton?.setOnClickListener {
             drawView?.clearCanvas()
+            eqn = ""
             predictedTextView?.text = getString(R.string.tfe_dc_prediction_text_placeholder);
+
         }
 
-        // Setup classification trigger so that it classify after every stroke drew
-        /*drawView?.setOnTouchListener { _, event ->
-            // As we have interrupted DrawView's touch event,
-            // we first need to pass touch events through to the instance for the drawing to show up
+        drawView?.setOnTouchListener { _, event ->
             drawView?.onTouchEvent(event)
 
-            // Then if user finished a touch event, run classification
             if (event.action == MotionEvent.ACTION_UP) {
                 classifyDrawing()
             }
 
             true
-        }*/
+        }
 
         classifyButton?.setOnClickListener {
-            predictedTextView?.text = classifyDrawing();
+            val symbol = classifyDrawing();
+            if(symbol == "-1"){
+                predictedTextView?.text = "Invalid Drawing Trying again"
+            }else{
+                eqn += getOperator(symbol)
+                predictedTextView?.text = eqn
+            }
+            drawView?.clearCanvas()
         }
 
 
+        calculate_button?.setOnClickListener {
+            try {
+                var result: Double;
+                val e : Expression = ExpressionBuilder(eqn).build()
+                result = e.evaluate();
+                eqn = "$eqn = ${result.toInt()}"
+                predictedTextView?.text = eqn
+            }catch (e: Exception) {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
 
         // Setup digit classifier
         digitClassifier.initializeInterpreter();
     }
 
+    private  fun getOperator(s: String): String{
+        if(s == "10"){
+            return "-";
+        }else if (s == "11"){
+            return "+";
+        }else if (s == "12"){
+            return "*";
+        }
+        return s;
+    }
 
     private fun classifyDrawing() : String {
         val bitmap = drawView?.getBitmap()
@@ -77,5 +114,55 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+    }
+
+    fun eval(str: String) : Int{
+
+        var posa = -1
+        var poss = -1
+        var posm = -1
+        var pos = -1
+
+        for(i in 0..str.length-1)
+        {
+            if(str.get(i) == '+')
+            {
+                posa = i
+                pos = i
+                break
+            }
+
+            if(str.get(i) == '*')
+            {
+                posm = i
+                pos = i
+                break
+            }
+
+            if(str.get(i) == '-')
+            {
+                poss = i
+                pos = i
+                break
+            }
+        }
+
+        var n1 = 0
+        var n2 = 0
+
+        n1 = (str.substring(0..pos - 1)).toInt()
+        n2 = (str.substring(pos + 1..str.length - 1)).toInt()
+
+        var ans : Int = 0
+
+        if(posa != -1)
+            ans = n1 + n2
+        else if(posm != -1)
+            ans = n1*n2
+        else
+            ans = n1 - n2
+
+        return ans;
+
     }
 }
--- app/src/main/java/com/example/inktomath/SymbolClassifier.kt
+++ app/src/main/java/com/example/inktomath/SymbolClassifier.kt
@@ -4,24 +4,26 @@ import android.content.Context
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.util.Log
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.flex.FlexDelegate
 import java.io.FileInputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.channels.FileChannel
-import org.tensorflow.lite.Interpreter
-import org.tensorflow.lite.flex.FlexDelegate
 
 class DigitClassifier(private val context: Context) {
     private var interpreter: Interpreter? = null
     var isInitialized = false
         private set
 
-    private var inputImageWidth: Int = 0 // will be inferred from TF Lite model
-    private var inputImageHeight: Int = 0 // will be inferred from TF Lite model
-    private var modelInputSize: Int = 0 // will be inferred from TF Lite model
+    private var inputImageWidth: Int = 28 // will be inferred from TF Lite model
+    private var inputImageHeight: Int = 28 // will be inferred from TF Lite model
+    private var modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth * inputImageHeight * PIXEL_SIZE
+// will be inferred from TF Lite model
 
     public fun initializeInterpreter() {
         // Load the TF Lite model
+
         val assetManager = context.assets
         val model = loadModelFile(assetManager)
 
@@ -31,10 +33,10 @@ class DigitClassifier(private val context: Context) {
 
 
         // Read input shape from model file
-        val inputShape = interpreter.getInputTensor(0).shape()
-        inputImageWidth = inputShape[1]
-        inputImageHeight = inputShape[2]
-        modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth * inputImageHeight * PIXEL_SIZE
+        val inputTensor = interpreter.getInputTensor(0)
+        val outputTensor = interpreter.getOutputTensor(0)
+
+        val tensor = "TENSOR"
 
         // Finish interpreter initialization
         this.interpreter = interpreter
@@ -59,22 +61,54 @@ class DigitClassifier(private val context: Context) {
         var startTime: Long
         var elapsedTime: Long
 
-        // Preprocessing: resize the input
         startTime = System.nanoTime()
         val resizedImage = Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true)
+        Log.d(TAG, "DIMS: ${resizedImage.width} ${resizedImage.height}")
+
         val byteBuffer = convertBitmapToByteBuffer(resizedImage)
+
+        Log.d(TAG, "BYTE BUFFER: ${byteBuffer.capacity().toString()}")
+
         elapsedTime = (System.nanoTime() - startTime) / 1000000
         Log.d(TAG, "Preprocessing time = " + elapsedTime + "ms")
 
         startTime = System.nanoTime()
         val result = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
-        interpreter?.run(byteBuffer, result)
+        try {
+            interpreter?.run(byteBuffer, result)
+        }catch (e: Exception){
+            Log.d("EXCEPTION INTERPRETER", e.toString());
+        }
         elapsedTime = (System.nanoTime() - startTime) / 1000000
         Log.d(TAG, "Inference time = " + elapsedTime + "ms")
+
+        Log.d(TAG, "Result =${result.contentDeepToString()}")
+
 
         return getOutputString(result[0])
     }
 
+    private fun twoDPixelArray(bitmap: Bitmap): Array<Array<Float>>{
+        val pixels = IntArray(inputImageWidth * inputImageHeight)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+        var pixelsBitmap: Array<Array<Float>> = Array(inputImageWidth){ Array(inputImageHeight){0.0f} }
+
+        for(i in 0 until inputImageWidth){
+            for(j in 0 until inputImageHeight){
+                pixelsBitmap[i][j] = normalize(pixels[(j * inputImageWidth) + i])
+            }
+        }
+        return pixelsBitmap
+    }
+
+    private fun normalize(pixelValue: Int): Float{
+        val r = (pixelValue shr 16 and 0xFF)
+        val g = (pixelValue shr 8 and 0xFF)
+        val b = (pixelValue and 0xFF)
+
+        return (r + g + b) / 3.0f / 255.0f
+    }
     private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
         val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
         byteBuffer.order(ByteOrder.nativeOrder())
@@ -88,7 +122,7 @@ class DigitClassifier(private val context: Context) {
             val b = (pixelValue and 0xFF)
 
             // Convert RGB to grayscale and normalize pixel value to [0..1]
-            val normalizedPixelValue = (r + g + b) / 3.0f / 255.0f
+            val normalizedPixelValue = ( r + g + b) / 3.0f / 255.0f
             byteBuffer.putFloat(normalizedPixelValue)
         }
 
@@ -96,18 +130,22 @@ class DigitClassifier(private val context: Context) {
     }
 
     private fun getOutputString(output: FloatArray): String {
-        val maxIndex = output.indices.maxBy { output[it] } ?: -1
-        return "Prediction Result: %d\nConfidence: %2f".format(maxIndex, output[maxIndex])
+        var maxIndex = output.indices.maxBy { output[it] } ?: -1
+        if (output[maxIndex] < 0.5){
+            maxIndex = -1
+        }
+        return maxIndex.toString()
+//        return "Prediction Result: %d\nConfidence: %2f".format(maxIndex, output[maxIndex])
     }
 
     companion object {
         private const val TAG = "DigitClassifier"
 
-        private const val MODEL_FILE = "mnist_model.tflite"
+        private const val MODEL_FILE = "mnist_final_4.tflite"
 
         private const val FLOAT_TYPE_SIZE = 4
         private const val PIXEL_SIZE = 1
 
-        private const val OUTPUT_CLASSES_COUNT = 10
+        private const val OUTPUT_CLASSES_COUNT = 13
     }
 }
