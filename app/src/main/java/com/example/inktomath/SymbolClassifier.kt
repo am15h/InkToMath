58cdc493ff82d95911ce3e636c4de2c23a6cb95b
am15h <amishgarg123@gmail.com>
2020-05-25 00:26:39 +0530

    add SymbolClassifier

--- app/src/main/java/com/example/inktomath/SymbolClassifier.kt
+++ app/src/main/java/com/example/inktomath/SymbolClassifier.kt
@@ -11,13 +11,13 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.channels.FileChannel
 
-class DigitClassifier(private val context: Context) {
+class SymbolClassifier(private val context: Context) {
     private var interpreter: Interpreter? = null
     var isInitialized = false
         private set
 
-    private var inputImageWidth: Int = 28 // will be inferred from TF Lite model
-    private var inputImageHeight: Int = 28 // will be inferred from TF Lite model
+    private var inputImageWidth: Int = 28
+    private var inputImageHeight: Int = 28
     private var modelInputSize = FLOAT_TYPE_SIZE * inputImageWidth * inputImageHeight * PIXEL_SIZE
 // will be inferred from TF Lite model
 
@@ -30,13 +30,6 @@ class DigitClassifier(private val context: Context) {
         val delegate = FlexDelegate();
         val options = Interpreter.Options().addDelegate(delegate)
         val interpreter = Interpreter(model, options)
-
-
-        // Read input shape from model file
-        val inputTensor = interpreter.getInputTensor(0)
-        val outputTensor = interpreter.getOutputTensor(0)
-
-        val tensor = "TENSOR"
 
         // Finish interpreter initialization
         this.interpreter = interpreter
@@ -58,57 +51,26 @@ class DigitClassifier(private val context: Context) {
             throw IllegalStateException("TF Lite Interpreter is not initialized yet.")
         }
 
-        var startTime: Long
-        var elapsedTime: Long
-
-        startTime = System.nanoTime()
         val resizedImage = Bitmap.createScaledBitmap(bitmap, inputImageWidth, inputImageHeight, true)
         Log.d(TAG, "DIMS: ${resizedImage.width} ${resizedImage.height}")
 
         val byteBuffer = convertBitmapToByteBuffer(resizedImage)
 
         Log.d(TAG, "BYTE BUFFER: ${byteBuffer.capacity().toString()}")
-
-        elapsedTime = (System.nanoTime() - startTime) / 1000000
-        Log.d(TAG, "Preprocessing time = " + elapsedTime + "ms")
-
-        startTime = System.nanoTime()
         val result = Array(1) { FloatArray(OUTPUT_CLASSES_COUNT) }
+
         try {
             interpreter?.run(byteBuffer, result)
         }catch (e: Exception){
             Log.d("EXCEPTION INTERPRETER", e.toString());
         }
-        elapsedTime = (System.nanoTime() - startTime) / 1000000
-        Log.d(TAG, "Inference time = " + elapsedTime + "ms")
 
         Log.d(TAG, "Result =${result.contentDeepToString()}")
-
 
         return getOutputString(result[0])
     }
 
-    private fun twoDPixelArray(bitmap: Bitmap): Array<Array<Float>>{
-        val pixels = IntArray(inputImageWidth * inputImageHeight)
-        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
 
-        var pixelsBitmap: Array<Array<Float>> = Array(inputImageWidth){ Array(inputImageHeight){0.0f} }
-
-        for(i in 0 until inputImageWidth){
-            for(j in 0 until inputImageHeight){
-                pixelsBitmap[i][j] = normalize(pixels[(j * inputImageWidth) + i])
-            }
-        }
-        return pixelsBitmap
-    }
-
-    private fun normalize(pixelValue: Int): Float{
-        val r = (pixelValue shr 16 and 0xFF)
-        val g = (pixelValue shr 8 and 0xFF)
-        val b = (pixelValue and 0xFF)
-
-        return (r + g + b) / 3.0f / 255.0f
-    }
     private fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
         val byteBuffer = ByteBuffer.allocateDirect(modelInputSize)
         byteBuffer.order(ByteOrder.nativeOrder())
@@ -135,7 +97,6 @@ class DigitClassifier(private val context: Context) {
             maxIndex = -1
         }
         return maxIndex.toString()
-//        return "Prediction Result: %d\nConfidence: %2f".format(maxIndex, output[maxIndex])
     }
 
     companion object {
